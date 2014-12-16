052afc5075fabd76aeef2de420c364dcbf1aefa1
Thirumal Venkat <me@thirumal.in>
2014-12-16 09:15:35 +0530

    Changes to accomodate grub also

--- homebrew_run.sh
+++ homebrew_run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+# Home made bootloader and kernel run script
+
 # Stop on error
 set -e
 
-# First compile everything
+# First compile the bootloader
 cd boot/
 make
 cd ../
 
 # Now copy second stage bootloader onto floppy
-losetup /dev/loop0 bin/boot.img
+losetup /dev/loop0 bin/homebrew_floppy.img
 mount /dev/loop0 /mnt -t msdos -o "fat=12"
 cp bin/KERNLD.SYS /mnt/
 umount /mnt
 losetup -d /dev/loop0
 
-# Will use the local bin/boot.img with KERNLOAD.SYS for now
-bochs
+# Run home made bootloader and kernel
+bochs -f homebrew_bochsrc.txt
