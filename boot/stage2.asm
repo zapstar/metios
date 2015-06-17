b1a8fb16a4e4867b4f431371da3db23c248390ee
Thirumal Venkat <me@thirumal.in>
2015-06-17 15:51:38 +0530

    Moved puts16 routine into an inc file

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -5,32 +5,19 @@
 ; @pre: We are loaded at linear address 0x20000
 ;******************************************************************************
 
-; Real mode
-bits	16
-
 ; Let's assume that our code is aligned to zero, we'll change it later on
 org	0
 
+; Start executing from main routine
 	jmp main
 
-;******************************************************************************
-; Debug print routine
-; @pre: SI should contain the address of the string
-;******************************************************************************
-bios_print_msg:
-.read_next:
-	lodsb		; Picks a fresh byte from SI into AL
-	cmp al, 0	; See if we have reached the end of the string
-	jz .debug_done	; If so, prepare to exit the routine
-	mov ah, 0x0e	; Print on TTY
-	int 0x10	; Make the INT call
-	jmp .read_next	; Get ready to print more
-.debug_done:
-	ret
+%include "stdio.inc"
 
 ;******************************************************************************
 ; Second Stage boot loader entry point
 ;******************************************************************************
+; Real mode
+bits	16
 
 main:
 ; Ensure that code segment and the data segment both are the same
@@ -38,7 +25,7 @@ main:
 	pop ds
 ; Print the initializing message for us
 	mov si, boot2_init_msg
-	call bios_print_msg
+	call puts16
 ; HALT for now. We'll look at what to do here later on
 	cli			; Disable interrupts
 	hlt			; Halt the system
@@ -46,5 +33,4 @@ main:
 ;******************************************************************************
 ; Global variables (DATA SECTION)
 ;******************************************************************************
-
 boot2_init_msg	db	"Intializing second stage bootloader...", 13, 10, 0
