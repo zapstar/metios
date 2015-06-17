44f0308c64aae55a090ec3471104d9fcb6d0765f
Thirumal Venkat <me@thirumal.in>
2015-06-17 21:25:16 +0530

    Added caret for Windows batch file

--- homebrew_run.cmd
+++ homebrew_run.cmd
@@ -12,37 +12,48 @@ set BOCHS=bochs
 rem set BOCHS=bochsdbg
 
 rem Check for NASM installation
-where nasm || echo NASM not in PATH && goto :error
+where nasm || ^
+echo NASM not in PATH && goto :error
 
 rem Check for Bochs installation
-where bochs || echo Bochs not in PATH && goto :error
+where bochs || ^
+echo Bochs not in PATH && goto :error
 
 rem Check for ImDisk installation
-where imdisk || echo ImDisk not in PATH && goto :error
+where imdisk || ^
+echo ImDisk not in PATH && goto :error
 
 rem Create the binary directory
-if not exist bin mkdir bin || echo Error making bin directory && goto :error
+if not exist bin mkdir bin || ^
+echo Error making bin directory && goto :error
 
 rem Change to boot loader directory
-cd boot || echo Did not find bootloader directory && goto :error
+cd boot || ^
+echo Did not find bootloader directory && goto :error
 
 rem Compile the first stage bootloader
-nasm boot.asm -f bin -o ..\bin\homebrew_floppy.img || echo Error building first stage bootloader && goto :error
+nasm boot.asm -f bin -o ..\bin\homebrew_floppy.img || ^
+echo Error building first stage bootloader && goto :error
 
 rem Compile the second stage bootloader
-nasm stage2.asm -f bin -o ..\bin\KERNLD.SYS || echo Error building second stage bootloader && goto :error
+nasm stage2.asm -f bin -o ..\bin\KERNLD.SYS || ^
+echo Error building second stage bootloader && goto :error
 
 rem Return back home
-cd .. || echo Could not return back home && goto :error
+cd .. || ^
+echo Could not return back home && goto :error
 
 rem Mount the floppy onto A drive
-imdisk -a -t file -o rw,rem,fd -m %DRIVE%: -f bin\homebrew_floppy.img || echo Error mounting floppy, please make sure A drive is free && goto :error
+imdisk -a -t file -o rw,rem,fd -m %DRIVE%: -f bin\homebrew_floppy.img || ^
+echo Error mounting floppy, please make sure A drive is free && goto :error
 
 rem Now copy second stage bootloader onto floppy
-copy bin\KERNLD.SYS %DRIVE%:\ || echo Error copying second stage bootloader. Remove floppy manually && goto :error
+copy bin\KERNLD.SYS %DRIVE%:\ || ^
+echo Error copying second stage bootloader. Remove floppy manually && goto :error
 
 rem Unmount the floppy, make it ready to boot
-imdisk -D -m %DRIVE%: || echo Error unmounting floppy, please force remove manually && goto :error
+imdisk -D -m %DRIVE%: || ^
+echo Error unmounting floppy, please force remove manually && goto :error
 
 rem Run home made bootloader and kernel
 %BOCHS% -q -f homebrew_bochsrc-win32.txt
